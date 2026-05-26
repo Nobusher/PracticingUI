cda0430656dcf0397a6da6cd7869b335d9372263
Nobusher <TheGreatestMind1337@yandex.ru>
2026-05-26 19:10:16 +0300

    Добавлена разметка

--- PracticingUI/ViewModels/MainWindowViewModel.cs
+++ PracticingUI/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,9 @@
 ﻿using Database;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualBasic;
 using System;
+using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace PracticingUI.ViewModels
 {
@@ -8,11 +11,36 @@ namespace PracticingUI.ViewModels
     {
         public string Greeting { get; } = "Welcome to Avalonia!";
         private readonly AppDbContext _db;
+
+        public ObservableCollection<FlightRow> Flights { get; } = [];
         public MainWindowViewModel(AppDbContext db)
         {
             _db = db;
-            var canConnect = db.Database.CanConnect();
-            Greeting = canConnect ? "Connected!" : "Failed to connect!";
+            LoadFlights();
         }
+
+        private void LoadFlights()
+        {
+            var flights = _db.Flights
+                .Include(f => f.DepartureAiport)
+                .Include(f => f.ArrivalAirport)
+                .ToList();
+
+            foreach(var f in flights)
+            {
+                Flights.Add(new FlightRow(
+                    f.FlightNumber,
+                    f.DepartureAiport.City,
+                    f.ArrivalAirport.City,
+                    f.Departuretime,
+                    f.ArrivalTime
+                    ));
+            }
+        }
+    }
+    public record FlightRow(string FlightNumber, string From, string To,
+        DateTime DepartureTime, DateTime ArrivalTime)
+    {
+        public string Status => "On schedule";
     }
 }
